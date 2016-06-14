cc48c8dc5157473e092c0c26a04aaf93ed8d5736
Otavio Guilherme Arruda dos Santos <otavioarrudasantos@gmail.com>
2016-06-14 12:00:43 -0300

    swapping values in one line o/

--- bubble_sort.go
+++ bubble_sort.go
@@ -5,9 +5,7 @@ func BubbleSort(data []int){
     for i := 0; i < length; i ++{
         for j := 0; j < length -1; j++{
             if data[j] > data[j+1]{
-                var aux = data[j]
-                data[j] = data[j+1]
-                data[j+1] = aux
+                data[j], data[j+1] = data[j+1], data[j]
             }
         }
     }
