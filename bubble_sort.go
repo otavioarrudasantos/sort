08fe59668e2269b85e10b0c0efd17fad0cd31f35
Otavio Guilherme Arruda dos Santos <otavioarrudasantos@gmail.com>
2016-06-14 11:43:12 -0300

    Assigned len(slice) to a variable. Removing semicolon

--- bubble_sort.go
+++ bubble_sort.go
@@ -1,12 +1,13 @@
 package sort
 
 func BubbleSort(data []int){
-    for i := 0; i < len(data); i ++{
-        for j := 0; j < len(data) -1; j++{
+    length := len(data);
+    for i := 0; i < length; i ++{
+        for j := 0; j < length -1; j++{
             if data[j] > data[j+1]{
                 var aux = data[j]
                 data[j] = data[j+1]
-                data[j+1] = aux;
+                data[j+1] = aux
             }
         }
     }
